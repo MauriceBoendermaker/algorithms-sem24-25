59e9a1f1b4f7da548b22028060726d8c0dde0f35
MauriceBoendermaker <mauriceboendermaker@gmail.com>
2025-02-10 20:09:40 +0100

    C# Display Variables

---
+++ W3Schools/C# Display Variables/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DisplayVariables
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string name = "John";
+            Console.WriteLine("Hello " + name);
+
+            string firstName = "John ";
+            string lastName = "Doe";
+            string fullName = firstName + lastName;
+            Console.WriteLine(fullName);
+
+            int x = 5;
+            int y = 6;
+            Console.WriteLine(x + y);
+        }
+    }
+}
