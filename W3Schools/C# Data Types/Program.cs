059a2841efe7049098eac0a9152eabeae83f7404
MauriceBoendermaker <mauriceboendermaker@gmail.com>
2025-02-10 20:24:38 +0100

    C# Data Types

---
+++ W3Schools/C# Data Types/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace DataTypes
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int myNum = 5;
+            double myDoubleNum = 5.99D;
+            char myLetter = 'D';
+            bool myBool = true;
+            string myText = "Hello";
+
+            Console.WriteLine(myNum);
+            Console.WriteLine(myDoubleNum);
+            Console.WriteLine(myLetter);
+            Console.WriteLine(myBool);
+            Console.WriteLine(myText);
+
+            float f1 = 35e3F;
+            double d1 = 12E4D;
+
+            Console.WriteLine(f1);
+            Console.WriteLine(d1);
+        }
+    }
+}
