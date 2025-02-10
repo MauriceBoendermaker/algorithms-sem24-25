d2e229167dbef852f264e14bb2680c21097b178a
MauriceBoendermaker <mauriceboendermaker@gmail.com>
2025-02-10 20:11:55 +0100

    C# Multiple Variables

---
+++ W3Schools/C# Multiple Variables/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MultipleVariables
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int x = 5, y = 6, z = 50;
+            Console.WriteLine(x + y + z);
+
+            int x1, y1, z1;
+            x1 = y1 = z1 = 50;
+            Console.WriteLine(x1 + y1 + z1);
+        }
+    }
+}
