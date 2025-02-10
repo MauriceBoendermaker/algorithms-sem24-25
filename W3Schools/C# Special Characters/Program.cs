75a89e7420dcdd25d7a3a8ead92ff3509ac0948a
MauriceBoendermaker <mauriceboendermaker@gmail.com>
2025-02-10 21:59:22 +0100

    C# Special Characters

---
+++ W3Schools/C# Special Characters/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SpecialCharacters
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string txt = "We are the so-called \"Vikings\" from the north.";
+            Console.WriteLine(txt);
+
+            string txt2 = "It\'s alright.";
+            Console.WriteLine(txt2);
+
+            string txt3 = "The character \\ is called backslash.";
+            Console.WriteLine(txt3);
+        }
+    }
+}
