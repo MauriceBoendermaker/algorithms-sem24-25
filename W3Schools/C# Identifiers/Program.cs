f520c76e2d43933ec1d3ed26a929f7cbb1a06d9a
MauriceBoendermaker <mauriceboendermaker@gmail.com>
2025-02-10 20:16:36 +0100

    C# Identifiers

---
+++ W3Schools/C# Identifiers/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Identifiers
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // Good
+            int minutesPerHour = 60;
+
+            // Ok, but not so easy to understand what m actually is
+            int m = 70;
+
+            Console.WriteLine(minutesPerHour);
+            Console.WriteLine(m);
+        }
+    }
+}
