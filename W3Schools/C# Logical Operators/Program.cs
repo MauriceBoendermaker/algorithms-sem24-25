893cc010f6bf1da85b024ac46ab39bc7595934f2
MauriceBoendermaker <mauriceboendermaker@gmail.com>
2025-02-10 21:24:11 +0100

    C# Logical Operators

---
+++ W3Schools/C# Logical Operators/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace LogicalOperators
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int x = 5;
+            int y = 10;
+            Console.WriteLine(x < 11 && y < 11);
+
+            int a = 10;
+            int b = 35;
+            Console.WriteLine(a > 2 || b < 40);
+        }
+    }
+}
