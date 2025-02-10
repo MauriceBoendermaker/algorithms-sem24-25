0467f24961b8728a0efea5c43546110bf1429c3c
MauriceBoendermaker <mauriceboendermaker@gmail.com>
2025-02-10 21:32:03 +0100

    C# Strings

---
+++ W3Schools/C# Strings/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Strings
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string greeting = "Hello";
+            Console.WriteLine(greeting);
+
+            string greeting2 = "Nice to meet you!";
+            Console.WriteLine(greeting2);
+
+            string txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            Console.WriteLine("The length of the txt string is: " + txt.Length);
+
+            string txt2 = "Hello World";
+            Console.WriteLine(txt2.ToUpper());
+            Console.WriteLine(txt2.ToLower());
+        }
+    }
+}
