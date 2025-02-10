0a9100ab1409c92529e6526dc46a0d861ef76f22
MauriceBoendermaker <mauriceboendermaker@gmail.com>
2025-02-10 20:04:12 +0100

    C# Variables

---
+++ W3Schools/C# Variables/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Variables
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string name = "John";
+            Console.WriteLine(name);
+
+            int myNum = 15;
+            Console.WriteLine(myNum);
+
+            int myNum2;
+            myNum2 = 25;
+            Console.WriteLine(myNum2);
+
+            int myNum3 = 35;
+            myNum3 = 40;
+            Console.WriteLine(myNum3);
+
+            double myDoubleNum = 5.99D;
+            Console.WriteLine(myDoubleNum);
+
+            char myLetter = 'D';
+            Console.WriteLine(myLetter);
+
+            bool myBool = true;
+            Console.WriteLine(myBool);
+
+            string myText = "Hello";
+            Console.WriteLine(myText);
+        }
+    }
+}
