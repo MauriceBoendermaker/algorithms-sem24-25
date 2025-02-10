1cb1516c26dac840b924ddc4bf9428a22bf8ed1d
MauriceBoendermaker <mauriceboendermaker@gmail.com>
2025-02-10 20:36:31 +0100

    C# User Input

---
+++ W3Schools/C# User Input/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace UserInput
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter username:");
+
+            string userName = Console.ReadLine();
+            Console.WriteLine("Username is: " + userName);
+
+
+            Console.WriteLine("Enter your age:");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Your age is: " + age);
+        }
+    }
+}
