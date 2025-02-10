d48490c7f62eee99144165f41300d6235c8bedc1
MauriceBoendermaker <mauriceboendermaker@gmail.com>
2025-02-10 21:51:55 +0100

    C# Access Strings

---
+++ W3Schools/C# Access Strings/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace AccessStrings
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string myString = "Hello";
+            Console.WriteLine(myString[0]); // H
+            Console.WriteLine(myString[1]); // e
+
+            Console.WriteLine(myString.IndexOf("e")); // 1
+
+
+            string name = "John Doe";
+
+            int charPos = name.IndexOf("D");
+
+            string lastName = name.Substring(charPos);
+
+            Console.WriteLine(lastName);
+        }
+    }
+}
