a1af80b65a5908b3bbd363a6768a92e0336c49bf
MauriceBoendermaker <mauriceboendermaker@gmail.com>
2025-02-10 19:59:13 +0100

    C# Comments

---
+++ W3Schools/C# Comments/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Comments
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            // This is a comment
+            Console.WriteLine("Hello World!"); // This is a comment
+
+            /*
+            This is a
+            multi-line comment
+            */
+        }
+    }
+}
