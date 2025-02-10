c12bc8bc00206ce4ef0f3b6fa3cd47f4a9ba4387
MauriceBoendermaker <mauriceboendermaker@gmail.com>
2025-02-10 21:38:50 +0100

    C# String Interpolation

---
+++ W3Schools/C# String Interpolation/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace StringInterpolation
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string firstName = "John";
+            string lastName = "Doe";
+            string name = $"My full name is: {firstName} {lastName}";
+            Console.WriteLine(name);
+        }
+    }
+}
