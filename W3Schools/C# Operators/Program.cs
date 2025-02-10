4344f0934014e21274a08d40f37453213f9acace
MauriceBoendermaker <mauriceboendermaker@gmail.com>
2025-02-10 20:41:20 +0100

    C# Operators

---
+++ W3Schools/C# Operators/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Operators
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int x = 100 + 50;
+            Console.WriteLine(x);
+
+            int sum1 = 100 + 50; // 150
+            int sum2 = sum1 + 250; // 400
+            int sum3 = sum2 + sum2; // 800
+
+            Console.WriteLine(sum3); // 800
+        }
+    }
+}
