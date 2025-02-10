aa3d90abfdb8143fdb742026bf171492ee911ecb
MauriceBoendermaker <mauriceboendermaker@gmail.com>
2025-02-10 20:06:32 +0100

    C# Constants

---
+++ W3Schools/C# Constants/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Constants
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            const int myNum = 15;
+            // myNum = 20; // error
+
+            Console.WriteLine(myNum);
+        }
+    }
+}
