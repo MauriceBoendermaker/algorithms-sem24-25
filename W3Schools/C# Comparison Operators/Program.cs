9b623b8b204e30fe151528ddeb906dcd782d05e0
MauriceBoendermaker <mauriceboendermaker@gmail.com>
2025-02-10 21:20:35 +0100

    C# Comparison Operators

---
+++ W3Schools/C# Comparison Operators/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ComparisonOperators
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int x = 5;
+            int y = 3;
+            Console.WriteLine(x > y); // True
+        }
+    }
+}
