d25df5390f10dcf452969bedb6e573395da82433
MauriceBoendermaker <mauriceboendermaker@gmail.com>
2025-02-10 21:28:38 +0100

    C# Math

---
+++ W3Schools/C# Math/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MathApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Math.Max(5, 10));
+
+            Console.WriteLine(Math.Min(2, 8));
+
+            Console.WriteLine(Math.Sqrt(64));
+
+            Console.WriteLine(Math.Abs(-4.7));
+
+            Console.WriteLine(Math.Round(9.99));
+        }
+    }
+}
