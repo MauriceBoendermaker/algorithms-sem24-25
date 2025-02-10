2cf35820601756f57da710e76e989ae0d4faec1e
MauriceBoendermaker <mauriceboendermaker@gmail.com>
2025-02-10 21:36:05 +0100

    C# String Concatenation

---
+++ W3Schools/C# String Concatenation/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace StringConcatenation
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string firstName = "John ";
+            string lastName = "Doe";
+            string name = firstName + lastName;
+            Console.WriteLine(name);
+
+            string name2 = string.Concat(firstName, lastName);
+            Console.WriteLine(name2);
+
+            int x = 10;
+            int y = 20;
+            int z = x + y;
+            Console.WriteLine(z); // 30
+
+            string a = "10";
+            string b = "20";
+            string c = a + b;
+            Console.WriteLine(c); // 1020
+        }
+    }
+}
