6311c745d71530b908848c394dddabeaf37286d6
MauriceBoendermaker <mauriceboendermaker@gmail.com>
2025-02-11 19:38:26 +0100

    C# While Loop

---
+++ W3Schools/C# While Loop/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace WhileLoop
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int i = 0;
+            while (i < 5)
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+
+            do
+            {
+                Console.WriteLine(i);
+                i++;
+            }
+            while (i < 5);
+        }
+    }
+}
