9a6d322aecbe2851ca98fb90d065e091d4c9b087
MauriceBoendermaker <mauriceboendermaker@gmail.com>
2025-02-11 18:55:26 +0100

    C# If ... Else

---
+++ W3Schools/C# IfElse/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace IfElse
+{
+    public class Program
+    {
+        public static void Main(string[] agrs)
+        {
+            if (20 > 18)
+            {
+                Console.WriteLine("20 is greater than 18");
+            }
+
+            int x = 20;
+            int y = 18;
+            if (x > y)
+            {
+                Console.WriteLine("x is greater than y");
+            }
+        }
+    }
+}
