31343d3998a565049fdf8dce4f0ed8de68a112d9
MauriceBoendermaker <mauriceboendermaker@gmail.com>
2025-02-11 19:59:24 +0100

    C# Break and Continue

---
+++ W3Schools/C# Break and Continue/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace BreakAndContinue
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                if (i == 4)
+                {
+                    break;
+                }
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < 10; i++)
+            {
+                if (i == 4)
+                {
+                    continue;
+                }
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine();
+
+            int x = 0;
+            while (x < 10)
+            {
+                Console.WriteLine(x);
+                x++;
+                if (x == 4)
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine();
+
+            int y = 0;
+            while (y < 10)
+            {
+                if (y == 4)
+                {
+                    y++;
+                    continue;
+                }
+                Console.WriteLine(y);
+                y++;
+            }
+        }
+    }
+}
