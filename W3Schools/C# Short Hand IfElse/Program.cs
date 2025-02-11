396707fa7f155d0bfded2e204ac014c4d8ac431c
MauriceBoendermaker <mauriceboendermaker@gmail.com>
2025-02-11 19:03:19 +0100

    C# Short Hand If...Else

---
+++ W3Schools/C# Short Hand IfElse/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ShortHandIfElse
+{
+    public class Program
+    {
+        public static void Main(string[] agrs)
+        {
+            int time = 20;
+            if (time < 18)
+            {
+                Console.WriteLine("Good day.");
+            }
+            else
+            {
+                Console.WriteLine("Good evening.");
+            }
+
+            string result = (time < 18) ? "Good day." : "Good evening.";
+            Console.WriteLine(result);
+        }
+    }
+}
