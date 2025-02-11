6d0aa381907a2a20b07ad1c26dd68dd1199f8555
MauriceBoendermaker <mauriceboendermaker@gmail.com>
2025-02-11 20:10:12 +0100

    C# Arrays

---
+++ W3Schools/C# Arrays/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Arrays
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] cars = { "Volvo", "BMW", "Ford", "Mazda" };
+
+            int[] myNum = { 10, 20, 30, 40 };
+
+            Console.WriteLine(cars[0]); // Volvo
+
+            Console.WriteLine();
+
+            cars[0] = "Opel";
+            Console.WriteLine(cars[0]);
+
+            Console.WriteLine();
+
+            Console.WriteLine(cars.Length); // 4
+
+            Console.WriteLine();
+
+            string[] fastCars = new string[4] { "Ferrari", "Porsche", "Lamborghini", "Koenigsegg" };
+
+            foreach (string car in fastCars)
+            {
+                Console.WriteLine(car);
+            }
+        }
+    }
+}
