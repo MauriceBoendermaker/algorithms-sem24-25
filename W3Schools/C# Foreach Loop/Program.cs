dab6b9aa69705242dd01e8b7187c41883d8e2cdc
MauriceBoendermaker <mauriceboendermaker@gmail.com>
2025-02-11 19:51:58 +0100

    C# Foreach Loop

---
+++ W3Schools/C# Foreach Loop/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ForeachLoop
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] cars = { "Volvo", "BMW", "Ford", "Mazda" };
+            foreach (string car in cars)
+            {
+                Console.WriteLine(car);
+            }
+        }
+    }
+}
