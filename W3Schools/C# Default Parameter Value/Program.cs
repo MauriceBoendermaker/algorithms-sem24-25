a857349175eb9d132388582991293e009844a113
MauriceBoendermaker <mauriceboendermaker@gmail.com>
2025-02-12 18:43:09 +0100

    C# Default Parameter Value

---
+++ W3Schools/C# Default Parameter Value/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace DefaultParameterValue
+{
+    public class Program
+    {
+        public static void MyMethod(string country = "Norway")
+        {
+            Console.WriteLine(country);
+        }
+
+        public static void Main(string[] args)
+        {
+            MyMethod("Sweden");
+            MyMethod("Netherlands");
+            MyMethod();
+            MyMethod("France");
+        }
+    }
+}
