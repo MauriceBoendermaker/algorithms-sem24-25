dd5fcbbf5a64331205aec4a6eb6873a267322fb0
MauriceBoendermaker <mauriceboendermaker@gmail.com>
2025-02-12 18:36:57 +0100

    C# Methods

---
+++ W3Schools/C# Methods/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Methods
+{
+    public class Program
+    {
+        public static void MyMethod()
+        {
+            Console.WriteLine("I just got printed in the console!");
+        }
+
+        public static void Main(string[] args)
+        {
+            MyMethod();
+            MyMethod();
+            MyMethod();
+        }
+    }
+}
