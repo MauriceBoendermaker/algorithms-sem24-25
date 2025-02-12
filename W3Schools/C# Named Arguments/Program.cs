6b8c5d837219f5667861336e97e873ab21f281f0
MauriceBoendermaker <mauriceboendermaker@gmail.com>
2025-02-12 20:05:46 +0100

    C# Named Arguments

---
+++ W3Schools/C# Named Arguments/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace NamedArguments
+{
+    public class Program
+    {
+        public static void MyMethod(string child1, string child2, string child3)
+        {
+            Console.WriteLine("The youngest chils is: " + child3);
+        }
+
+        public static void Main(string[] args)
+        {
+            MyMethod(child3: "John", child1: "Liam", child2: "Liam");
+        }
+    }
+}
