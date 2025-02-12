b66e64511dcea8ed37662cac9423f7d6a70f69c3
MauriceBoendermaker <mauriceboendermaker@gmail.com>
2025-02-12 20:03:22 +0100

    C# Return Values

---
+++ W3Schools/C# Return Values/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ReturnValues
+{
+    public class Program
+    {
+        public static int MyMethod(int x, int y)
+        {
+            return x + y;
+        }
+
+        public static void Main(string[] args)
+        {
+            int z = MyMethod(5, 3);
+            Console.WriteLine(z);
+        }
+    }
+}
