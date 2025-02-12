c7a2d7f85409a29f4df60d7446f44dce069ca0dd
MauriceBoendermaker <mauriceboendermaker@gmail.com>
2025-02-12 18:23:17 +0100

    C# Multidimensional Arrays

---
+++ W3Schools/C# Multidimensional Arrays/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace MultidimensionalArrays
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[,] numbers = { { 1, 2, 3 }, { 4, 5, 6 } };
+            Console.WriteLine(numbers[0, 2]); // 3
+
+            Console.WriteLine();
+
+            numbers[0, 0] = 100;
+            Console.WriteLine(numbers[0, 0]); // 100
+
+            Console.WriteLine();
+
+            foreach (int number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+
+            Console.WriteLine();
+
+            for (int i = 0; i < numbers.GetLength(0); i++)
+            {
+                Console.WriteLine("Outer loop: " + i);
+
+                for (int j = 0; j < numbers.GetLength(1); j++)
+                {
+                    Console.WriteLine(" Inner loop: " + numbers[i, j]);
+                }
+            }
+        }
+    }
+}
