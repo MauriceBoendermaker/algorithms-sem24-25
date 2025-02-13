601fb60365fdcc7f78ed3d386e8830e6c4c7be84
MauriceBoendermaker <mauriceboendermaker@gmail.com>
2025-02-13 14:03:54 +0100

    C# Constructors

---
+++ W3Schools/C# Constructors/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Constructors
+{
+    public class Car
+    {
+        public string model; // Create a field
+        public string color;
+        public int year;
+
+        // Create a class constructor
+        public Car(string modelName, string modelColor, int modelYear)
+        {
+            model = modelName;
+            color = modelColor;
+            year = modelYear;
+        }
+
+        public static void Main(string[] args)
+        {
+            Car Ford = new Car("Mustang", "Red", 1969); // Create an object
+            Console.WriteLine(Ford.color + " " + Ford.year + " " + Ford.model);
+        }
+    }
+}
