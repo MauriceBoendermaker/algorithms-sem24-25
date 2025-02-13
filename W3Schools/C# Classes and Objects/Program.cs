78896d5c6997a5b38d78255d49c816c4eb6ab05b
MauriceBoendermaker <mauriceboendermaker@gmail.com>
2025-02-13 13:10:30 +0100

    C# Classes and Objects

---
+++ W3Schools/C# Classes and Objects/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ClassesAndObjects
+{
+    public class Car
+    {
+        string color = "red";
+
+        public static void Main(string[] args)
+        {
+            Car myObj = new Car();
+            Console.WriteLine(myObj.color);
+        }
+    }
+}
