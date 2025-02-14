67241475c2791ad88ea0ee57845e8994c108ccbc
MauriceBoendermaker <mauriceboendermaker@gmail.com>
2025-02-14 14:32:00 +0100

    C# Properties (Get and Set)

---
+++ W3Schools/C# Properties (Get and Set)/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Properties
+{
+    public class Person
+    {
+        private string name; // field
+        private int age;
+
+        public string Name // property
+        {
+            get { return name; } // get method
+            set { name = value; } // set method
+        }
+
+        public int Age { get; set; }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Person myObj = new Person();
+            myObj.Name = "Liam";
+            myObj.Age = 28;
+
+            Console.WriteLine(myObj.Name + " is " + myObj.Age);
+        }
+    }
+}
