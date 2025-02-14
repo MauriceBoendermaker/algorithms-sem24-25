b38425c68677502191c628c1589d8360f8403753
MauriceBoendermaker <mauriceboendermaker@gmail.com>
2025-02-14 15:00:01 +0100

    C# Inheritance

---
+++ W3Schools/C# Inheritance/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Inheritance
+{
+    class Vehicle // base class
+    {
+        public string brand = "Ford"; // field
+        public void honk() // method
+        {
+            Console.WriteLine("Tuut, tuut!");
+        }
+    }
+
+    class Car : Vehicle // derived class
+    {
+        public string modelName = "Mustang"; // field
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar = new Car(); // create object
+
+            myCar.honk(); // call the method
+
+            Console.WriteLine(myCar.brand + " " + myCar.modelName);
+        }
+    }
+}
