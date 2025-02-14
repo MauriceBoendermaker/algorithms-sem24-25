609b725e829316b2fcc9ed24f608a755469bdf5e
MauriceBoendermaker <mauriceboendermaker@gmail.com>
2025-02-14 15:09:58 +0100

    C# Polymorphism

---
+++ W3Schools/C# Polymorphism/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Polymorphism
+{
+    class Animal // base class
+    {
+        public virtual void animalSound()
+        {
+            Console.WriteLine("The animal makes a sound");
+        }
+    }
+
+    class Pig : Animal // derived class
+    {
+        public override void animalSound()
+        {
+            Console.WriteLine("The pig says: wee wee");
+        }
+    }
+
+    class Dog : Animal // derived class
+    {
+        public override void animalSound()
+        {
+            Console.WriteLine("The dog says: bow bow");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal myAnimal = new Animal(); // create an animal object
+            Animal myPig = new Pig(); // create a pig object
+            Animal myDog = new Dog(); // create a dog object
+
+            myAnimal.animalSound();
+            myPig.animalSound();
+            myDog.animalSound();
+        }
+    }
+}
