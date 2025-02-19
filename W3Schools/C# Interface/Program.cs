d0b237cd1ff45d77915eab994edecb2dcbd32448
MauriceBoendermaker <mauriceboendermaker@gmail.com>
2025-02-19 13:29:39 +0100

    C# Interface

---
+++ W3Schools/C# Interface/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net;
+
+namespace Interface
+{
+    interface IAnimal
+    {
+        void animalSound(); // interface method (thus without body)
+    }
+
+    // Pig "implements" the IAnimal interface
+    class Pig : IAnimal
+    {
+        public void animalSound()
+        {
+            // The body of animalSound() is provided here
+            Console.WriteLine("The pig says: wee wee");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pig myPig = new Pig(); // Create a pig object
+            myPig.animalSound();
+        }
+    }
+}
