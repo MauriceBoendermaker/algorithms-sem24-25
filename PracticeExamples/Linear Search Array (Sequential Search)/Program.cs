b246fa8551ae1a0831385794037a7bdfa265097c
MauriceBoendermaker <mauriceboendermaker@gmail.com>
2025-02-23 13:06:10 +0100

    Linear / Sequential search

---
+++ PracticeExamples/Linear Search Array (Sequential Search)/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SequentialSearch
+{
+    public class Program
+    {
+        public static int[] arr = { 0, 3, 4, 5, 6, 7 };
+
+        // Key is the value we are searching for
+        public static int SequentialSearch(int[] arr, int key)
+        {
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == key)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(SequentialSearch(arr, 6));
+        }
+    }
+}
