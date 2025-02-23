55c428b822e98dec1649903b76ecfba9c50bfec7
MauriceBoendermaker <mauriceboendermaker@gmail.com>
2025-02-23 14:01:11 +0100

    Binary Search

---
+++ PracticeExamples/Binary Search/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace BinarySearch
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] numbers = { 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 21 };
+            int target = 22;
+
+            Console.WriteLine(BinarySearch(numbers, target));
+        }
+
+        static int BinarySearch(int[] array, int target)
+        {
+            int left = 0;
+            int right = array.Length - 1;
+
+            while (left <= right)
+            {
+                int mid = (left + right) / 2;
+
+                if (array[mid] == target)
+                {
+                    return mid;
+                }
+                else if (array[mid] < target)
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    right = mid - 1;
+                }
+            }
+            return -1;
+        }
+    }
+}
