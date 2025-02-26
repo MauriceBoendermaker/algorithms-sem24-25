12766b4231814b33023c6e27bbffd2e88b16b110
MauriceBoendermaker <mauriceboendermaker@gmail.com>
2025-02-26 13:13:52 +0100

    Intermediate Level #3

---
+++ PracticeExamples/More binary search/Intermediate Level #3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static int BinarySearchDescending(int[] arr, int target)
+    {
+        int left = 0;
+        int right = arr.Length - 1;
+
+        while (left <= right)
+        {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] == target)
+                return mid;
+
+            if (arr[mid] > target)
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+
+        return -1;
+    }
+
+    static void Main()
+    {
+        int[] sortedArray = { 20, 18, 15, 12, 10, 5, 1 };
+        int target = 18;
+        int result = BinarySearchDescending(sortedArray, target);
+        Console.WriteLine("Index: " + result);
+    }
+}
