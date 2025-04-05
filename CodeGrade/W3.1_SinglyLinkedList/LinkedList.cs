f783a464fe2dff29f446a0464760493300e00266
MauriceBoendermaker <mauriceboendermaker@gmail.com>
2025-04-05 16:17:20 +0200

    W3.1_SinglyLinkedList

--- CodeGrade/W3.1_SinglyLinkedList/LinkedList.cs
+++ CodeGrade/W3.1_SinglyLinkedList/LinkedList.cs
@@ -5,45 +5,113 @@ namespace Solution;
 public class SinglyLinkedList<T> : ILinkedList<T> where T : IComparable<T>
 {
     public SingleNode<T>? Head;
+    private SingleNode<T>? Tail;
     private int count;
 
     public SinglyLinkedList()
     {
-        throw new NotImplementedException();
+        Head = null;
+        Tail = null;
+        count = 0;
     }
 
     public int Count => count;
 
     public void AddFirst(T value)
     {
-        throw new NotImplementedException();
+        var newNode = new SingleNode<T>(value, Head);
+        Head = newNode;
+        if (Tail == null) Tail = Head;
+        count++;
     }
 
     public void AddLast(T value)
     {
-        throw new NotImplementedException();
+        var newNode = new SingleNode<T>(value);
+        if (Head == null)
+        {
+            Head = newNode;
+            Tail = newNode;
+        }
+        else
+        {
+            Tail!.Next = newNode;
+            Tail = newNode;
+        }
+        count++;
     }
 
     public bool Remove(T value)
     {
-        throw new NotImplementedException();
+        if (Head == null) return false;
+
+        if (Head.Value.CompareTo(value) == 0)
+        {
+            Head = Head.Next;
+            if (Head == null) Tail = null;
+            count--;
+            return true;
+        }
+
+        SingleNode<T> current = Head;
+        while (current.Next != null && current.Next.Value.CompareTo(value) != 0)
+        {
+            current = current.Next;
+        }
+
+        if (current.Next == null) return false;
+
+        current.Next = current.Next.Next;
+        if (current.Next == null) Tail = current;
+        count--;
+        return true;
     }
 
     public SingleNode<T>? Search(T value)
     {
-        throw new NotImplementedException();
+        SingleNode<T>? current = Head;
+        while (current != null)
+        {
+            if (current.Value.CompareTo(value) == 0)
+                return current;
+            current = current.Next;
+        }
+        return null;
     }
 
-    public bool Contains(T value) => throw new NotImplementedException();
+    public bool Contains(T value)
+    {
+        return Search(value) != null;
+    }
 
     public void AddSorted(T value)
     {
-        throw new NotImplementedException();
+        var newNode = new SingleNode<T>(value);
+        if (Head == null || Head.Value.CompareTo(value) >= 0)
+        {
+            newNode.Next = Head;
+            Head = newNode;
+            if (Tail == null) Tail = newNode;
+        }
+        else
+        {
+            SingleNode<T> current = Head;
+            while (current.Next != null && current.Next.Value.CompareTo(value) < 0)
+            {
+                current = current.Next;
+            }
+            newNode.Next = current.Next;
+            current.Next = newNode;
+            if (newNode.Next == null) Tail = newNode;
+        }
+        count++;
     }
 
     public void Clear()
     {
-        throw new NotImplementedException();
+        Head = null;
+        Tail = null;
+        count = 0;
     }
 
     public IEnumerator<T> GetEnumerator()
@@ -60,5 +128,4 @@ public class SinglyLinkedList<T> : ILinkedList<T> where T : IComparable<T>
     {
         return GetEnumerator();
     }
-
 }
