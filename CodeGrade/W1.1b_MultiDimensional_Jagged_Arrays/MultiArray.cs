5d32ed699bb112c74c9c5a55721976dad6ffb3e5
MauriceBoendermaker <mauriceboendermaker@gmail.com>
2025-04-11 15:55:40 +0200

    Changes to W1.1b_MultiDimensional_Jagged_Arrays

--- CodeGrade/W1.1b_MultiDimensional_Jagged_Arrays/MultiArray.cs
+++ CodeGrade/W1.1b_MultiDimensional_Jagged_Arrays/MultiArray.cs
@@ -6,19 +6,62 @@ public class MultiArray : IMultiArray
 {
     public static T[]? RowSum<T>(T[,] arr2D) where T : INumber<T>
     {
-        //ToDo
-        throw new NotImplementedException();
+        T[] sum = new T[arr2D.GetLength(0)];
+
+        for (int i = 0; i < arr2D.GetLength(0); i++)
+        {
+            sum[i] = T.Zero;
+
+            for (int j = 0; j < arr2D.GetLength(1); j++)
+            {
+                sum[i] += arr2D[i, j];
+            }
+        }
+        return sum;
     }
+
     public static T[]? ColSum<T>(T[,] arr2D) where T : INumber<T>
     {
-        //ToDo
-        throw new NotImplementedException();
+        T[] sum = new T[arr2D.GetLength(1)];
+
+        for (int i = 0; i < arr2D.GetLength(1); i++)
+        {
+            sum[i] = T.Zero;
+
+            for (int j = 0; j < arr2D.GetLength(0); j++)
+            {
+                sum[i] += arr2D[i, j];
+            }
+        }
+        return sum;
     }
 
     public static Tuple<int, T>? MaxRowIndexSum<T>(T[][] arrJagged) where T : INumber<T>
     {
-        //ToDo
-        throw new NotImplementedException();
+        T maxSum = T.Zero;
+        int maxIndex = -1;
+
+        for (int i = 0; i < arrJagged.Length; i++)
+        {
+            if (arrJagged[i] == null || arrJagged[i].Length == 0)
+            {
+                continue;
+            }
+
+            T sum = T.Zero;
+
+            foreach (T elem in arrJagged[i])
+            {
+                sum += elem;
+            }
+
+            if (sum > maxSum)
+            {
+                maxSum = sum;
+                maxIndex = i;
+            }
+        }
+        return maxIndex != -1 ? Tuple.Create(maxIndex, maxSum) : null;
     }
 
     public static T?[] MaxCol<T>(T[][] arrJagged) where T : INumber<T>
@@ -29,8 +72,23 @@ public class MultiArray : IMultiArray
 
     public static T[][]? Split<T>(Tuple<T, T, T>[] input)
     {
-        //ToDo
-        throw new NotImplementedException();
+        if (input == null || input.Length == 0)
+        {
+            return null;
+        }
+
+        T[][] jaggedArray = new T[3][];
+        jaggedArray[0] = new T[input.Length];
+        jaggedArray[1] = new T[input.Length];
+        jaggedArray[2] = new T[input.Length];
+
+        for (int i = 0; i < input.Length; i++)
+        {
+            jaggedArray[0][i] = input[i].Item1;
+            jaggedArray[1][i] = input[i].Item2;
+            jaggedArray[2][i] = input[i].Item3;
+        }
+        return jaggedArray;
     }
 
     public static T[,]? Zip<T>(T[] a, T[] b)
